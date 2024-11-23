c39a19cd0a4e5a16da870cd94311b6dad57e2d0e
Divya7685 <asdivya0503@gmail.com>
2024-11-23 23:36:02 +0530

    built in types

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -3,4 +3,25 @@ let age = 19;
 if (age < 60)
     age += 20;
 console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [];
+console.log(numbers);
+let user = [9, 'Divya'];
+console.log(user[0]);
+console.log(user[1]);
+;
+let mySize = 2;
+console.log(mySize);
+function calculateTax(income, taxYear = 2024) {
+    if (taxYear < 2024)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000);
 //# sourceMappingURL=index.js.map
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,5 +1,47 @@
 let age: number = 19;
 if (age < 60)
-    age+=20;
+    age += 20;
 console.log(age);
+
+
+// any type
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
+function render(document: any) {
+    console.log(document);
+}
+
+// array
+let numbers: number[] = [];
+console.log(numbers)
+
+// tuples[key,value]
+let user = [9, 'Divya'];
+console.log(user[0]);
+console.log(user[1]);
+
+// enum
+// one way
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+// PascalCase(first letter should be capital)
+const enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// function
+function calculateTax(income: number, taxYear = 2024): number {
+    if (taxYear < 2024)
+        return income * 1.2;
+    return income * 1.3;
+
+}
+calculateTax(10_000);
+
+
+
 
