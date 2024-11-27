b55f71970b5a8cb312e039c600d9566322847d5e
Divya7685 <asdivya0503@gmail.com>
2024-11-27 18:25:02 +0530

    basics of tailwindCSS

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      {/* <section className="m-12 h-screen bg-twitter-blue flex items-center justify-center p-12 text-center text-3xl
+       md:p-14 lg:p-16 xl:p-20">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt sequi impedit voluptatem aliquid, sapiente consectetur ex temporibus eaque, non provident sunt alias neque possimus esse unde? Odit delectus magnam cum?
+        </p>
+      </section> */}
+      <div className="card">
+        This is a cart
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default App
