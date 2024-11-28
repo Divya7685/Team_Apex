bdc81b5cb1628bc75e9e9d28a8bce9522b23219b
Divya7685 <asdivya0503@gmail.com>
2024-11-28 08:53:33 +0530

    Home,CountryDetails added

--- TS_Project/weatherapp/src/App.tsx
+++ TS_Project/weatherapp/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import {Route,Routes} from 'react-router-dom';
+import {Home} from './components/Home';
+import { CountryDetails } from './components/CountryDetails';
 
 
 function App() {
   
     return (
-      <div >
-        project
-       
-      </div>
+      <div className="App">
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:name" element={<CountryDetails />} />
+      </Routes>
+  </div>
   
   );
 }
--- TS_Project/weatherapp/src/index.tsx
+++ TS_Project/weatherapp/src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(<React.StrictMode>
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+</React.StrictMode>,
+document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
