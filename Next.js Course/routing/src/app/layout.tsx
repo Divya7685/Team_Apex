b1256ea3effccd40085c6301906c54a041c81174
Divya7685 <asdivya0503@gmail.com>
2024-12-08 21:44:06 +0530

    Nested Layouts

--- Next.js Course/routing/src/app/layout.tsx
+++ Next.js Course/routing/src/app/layout.tsx
@@ -21,9 +21,12 @@ export default function RootLayout({
           <p>header</p>
             </header>
             {children}
-            <footer>
-              <p>
-                Footer</p>
+            <footer
+            style={{
+              backgroundColor:"ghostwhite",
+              padding:"1rem",
+            }}>
+              <p>Footer</p>
             </footer></body>
     </html>
   )
